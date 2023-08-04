29408e63f0530c50a92d81e07ea0d23c05b2d92e
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-08-04 17:00:34 +0200

    Setting up jwt

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
