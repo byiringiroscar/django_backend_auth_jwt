257cd617dba8f9d888c69fa16fddc8992aefe3e8
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-08-05 20:44:11 +0200

    Customize access token response

--- base/api/urls.py
+++ base/api/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from .views import *
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
 
 urlpatterns = [
     path('', getRoutes),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
