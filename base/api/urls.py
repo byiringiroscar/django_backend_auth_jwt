bfa307e3b322364f9c0953ad48833647d1a46c95
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-08-05 20:11:55 +0200

    Setting up jwt configuration

--- base/api/urls.py
+++ base/api/urls.py
@@ -8,6 +8,6 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('', getRoutes),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
